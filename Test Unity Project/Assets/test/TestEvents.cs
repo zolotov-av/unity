08b9479942eac8dba10f2b1269deb1c67128b497
Alex V. Zolotov <shade@shamangrad.net>
2017-10-09 19:26:39 +0300

    + скрипт выводящий в лог фазы инициализации (Awake, Start, OnDestroy)

---
+++ Test Unity Project/Assets/test/TestEvents.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestEvents: MonoBehaviour
+{
+	
+	void Start()
+	{
+		Scene scene = SceneManager.GetActiveScene();
+		Debug.Log(gameObject.name + ".Start(), scene[" + scene.name + "]");
+	}
+	
+	void Awake()
+	{
+		Scene scene = SceneManager.GetActiveScene();
+		Debug.Log(gameObject.name + ".Awake(), scene[" + scene.name + "]");
+	}
+	
+	void OnDestroy()
+	{
+		Scene scene = SceneManager.GetActiveScene();
+		Debug.Log(gameObject.name + ".OnDestroy(), scene[" + scene.name + "]");
+	}
+	
+}
