ac176fae968d101504ecb15e250b34c28e71846d
Alex V. Zolotov <shade@shamangrad.net>
2017-10-16 14:35:53 +0300

    + простой скрипт переключения сцены для отладки

---
+++ Test Unity Project/Assets/test/RunScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RunScene : MonoBehaviour
+{
+	public string sceneName = "";
+	
+	void Start()
+	{
+		if ( sceneName == null ) return;
+		if ( sceneName == "" ) return;
+		
+		
+		if ( sceneName != null )
+		{
+			SceneManager.LoadScene(sceneName);
+		}
+	}
+}
