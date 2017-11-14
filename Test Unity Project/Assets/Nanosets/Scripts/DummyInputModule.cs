936e2242fbac26d1745509f2b92599fc1c1c2279
Alex V. Zolotov <shade@shamangrad.net>
2017-11-14 16:32:10 +0300

    + Фиктивный модуль ввода для EventSystem

---
+++ Test Unity Project/Assets/Nanosets/Scripts/DummyInputModule.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Nanosoft
+{
+
+/**
+ * Фиктивный модуль ввода для EventSystem
+ */
+[AddComponentMenu("Event/Dummy Input Module")]
+public class DummyInputModule: BaseInputModule
+{
+	
+	/**
+	 * Обработка ввода (ничего не делать)
+	 */
+	public override void Process()
+	{
+	}
+	
+} // class DummyInputModule
+
+} // namespace Nanosoft
