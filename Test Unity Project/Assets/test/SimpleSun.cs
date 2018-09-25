4f91f0049acb5cfc3965314f0d194960f8e7208a
Alex V. Zolotov <shade@shamangrad.net>
2018-09-25 23:17:14 +0300

    gradient for SimpleSun.cs

--- Test Unity Project/Assets/test/SimpleSun.cs
+++ Test Unity Project/Assets/test/SimpleSun.cs
@@ -11,19 +11,31 @@ public class SunLight: MonoBehaviour
 {
 	
 	private float curr;
+	private Light light;
+	
 	public float speed = 10f;
+	public Gradient gradient;
 	
 	void Awake()
 	{
 		curr = 0f;
+		light = GetComponent<Light>();
 	}
 	
 	void Update()
 	{
 		
 		curr += speed * Time.deltaTime;
-		while ( curr > 360f ) curr -= 360f;
+		while ( curr > 180f ) curr -= 360f;
+		while ( curr < -180f ) curr += 360f;
 		transform.eulerAngles = new Vector3(curr, 0f, 0f);
+		
+		float diff = curr - 90f;
+		while ( diff > 180f ) diff -= 360f;
+		while ( diff < -180f ) diff += 360f;
+		
+		float len = (diff + 180f) / 360f;
+		light.color = gradient.Evaluate(len);
 	}
 	
 }
