c70897c66d7893d84cc657513c7e90b33c4a83e1
Alex V. Zolotov <shade@shamangrad.net>
2018-09-25 22:53:17 +0300

    + SimpleSun.cs

---
+++ Test Unity Project/Assets/test/SimpleSun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//#pragma warning disable 0414
+
+namespace Nanosoft
+{
+
+public class SunLight: MonoBehaviour
+{
+	
+	private float curr;
+	public float speed = 10f;
+	
+	void Awake()
+	{
+		curr = 0f;
+	}
+	
+	void Update()
+	{
+		
+		curr += speed * Time.deltaTime;
+		while ( curr > 360f ) curr -= 360f;
+		transform.eulerAngles = new Vector3(curr, 0f, 0f);
+	}
+	
+}
+
+} // namespace Nanosoft
